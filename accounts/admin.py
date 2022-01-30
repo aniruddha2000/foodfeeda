253d3c0cf26b581e4331f8fcc586090f86b2e790
Aniruddha Basak <codewithaniruddha@gmail.com>
2022-01-30 12:46:18 +0530

    Add Donner and NGO field to the admin pannel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,7 @@
 from django.contrib import admin
-from .models import CustomUser, NGO
+from .models import CustomUser, NGO, Donner
 
 
-# from .models import NGO,Donner
-# # Register your models here.
-# admin.site.register(Donner)
-admin.site.register(NGO)
 admin.site.register(CustomUser)
+admin.site.register(NGO)
+admin.site.register(Donner)
