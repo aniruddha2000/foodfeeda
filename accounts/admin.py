cf7c20b7464522d07c51f97745493c65d2694f00
Aniruddha Basak <codewithaniruddha@gmail.com>
2022-01-30 12:05:19 +0530

    Add NGO field to admin pannel

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
-from .models import CustomUser
+from .models import CustomUser, NGO
 
 
 # from .models import NGO,Donner
 # # Register your models here.
-# admin.site.register(NGO)
 # admin.site.register(Donner)
+admin.site.register(NGO)
 admin.site.register(CustomUser)
