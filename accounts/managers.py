be9a5b23171ff5e22ea32644f626110a19211707
Aniruddha Basak <codewithaniruddha@gmail.com>
2022-01-30 12:06:53 +0530

    Remove staff_user from models
    Remove staff_user field from Custom User model and add NGO database
    with description field

--- accounts/managers.py
+++ accounts/managers.py
@@ -1,6 +1,6 @@
 from django.contrib.auth.base_user import BaseUserManager
 from django.utils.translation import ugettext_lazy as _
-    
+
 
 class CustomUserManager(BaseUserManager):
     """
--- accounts/models.py
+++ accounts/models.py
@@ -1,115 +1,60 @@
+from pydoc import describe
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 from django.utils.translation import ugettext_lazy as _
 
 from .managers import CustomUserManager
 
-class CustomUser(AbstractUser):
-    username = None
-    email = models.EmailField(_('email address'), unique=True)
 
-    USERNAME_FIELD = 'email'
-    REQUIRED_FIELDS = []
+class CustomUser(AbstractUser):
+
+    username = None
+    email = models.EmailField(_("email address"), unique=True)
+    name = models.CharField(max_length=100)
+
+    ## TODO: Location, contact
+
+    is_admin = models.BooleanField(default=False)
+    # is_staff = models.BooleanField(default=False)
+    is_superuser = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
 
     objects = CustomUserManager()
 
-    spouse_name = models.CharField(blank=True, max_length=100)
-    date_of_birth = models.DateField(blank=True, null=True)
-    
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = ["name"]
+
+    def get_full_name(self):
+        # The user is identified by their email address
+        return self.email
+
+    def get_short_name(self):
+        # The user is identified by their email address
+        return self.email
 
     def __str__(self):
         return self.email
 
-# class NGO(AbstractUser):
-#     username = None
-#     email = models.EmailField(_("email address"), unique=True)
-#     name = models.CharField(max_length=100)
-#     ## TODO: Location, contact
-#     is_admin = models.BooleanField(default=False)
-#     is_staff = models.BooleanField(default=False)
-#     is_superuser = models.BooleanField(default=False)
-#     is_active = models.BooleanField(default=True)
+    def has_perm(self, perm, obj=None):
+        "Does the user have a specific permission?"
+        # Simplest possible answer: Yes, always
+        return True
 
-#     objects = CustomUserManager()
+    def has_module_perms(self, app_label):
+        "Does the user have permissions to view the app `app_label`?"
+        # Simplest possible answer: Yes, always
+        return True
 
-#     USERNAME_FIELD = "email"
-#     REQUIRED_FIELDS = ["email", "name"]
+    # @property
+    # def is_staff(self):
+    #     "Is the user a member of staff?"
+    #     return self.staff
 
-#     def get_full_name(self):
-#         # The user is identified by their email address
-#         return self.email
-
-#     def get_short_name(self):
-#         # The user is identified by their email address
-#         return self.email
-
-#     def __str__(self):
-#         return self.email
-
-#     def has_perm(self, perm, obj=None):
-#         "Does the user have a specific permission?"
-#         # Simplest possible answer: Yes, always
-#         return True
-
-#     def has_module_perms(self, app_label):
-#         "Does the user have permissions to view the app `app_label`?"
-#         # Simplest possible answer: Yes, always
-#         return True
-
-#     @property
-#     def is_staff(self):
-#         "Is the user a member of staff?"
-#         return self.staff
-
-#     @property
-#     def is_admin(self):
-#         "Is the user a admin member?"
-#         return self.admin
+    @property
+    def is_admin(self):
+        "Is the user a admin member?"
+        return self.admin
 
 
-# class Donner(AbstractUser):
-#     username = None
-#     email = models.EmailField(_("email address"), unique=True)
-#     name = models.CharField(max_length=100)
-#     coins = models.IntegerField(default=5)
-#     ## TODO: contact
-#     is_admin = models.BooleanField(default=False)
-#     is_staff = models.BooleanField(default=False)
-#     is_superuser = models.BooleanField(default=False)
-#     is_active = models.BooleanField(default=True)
-
-#     objects = CustomUserManager()
-
-#     USERNAME_FIELD = "email"
-#     REQUIRED_FIELDS = ["email", "name"]
-
-#     def get_full_name(self):
-#         # The user is identified by their email address
-#         return self.email
-
-#     def get_short_name(self):
-#         # The user is identified by their email address
-#         return self.email
-
-#     def __str__(self):
-#         return self.email
-
-#     def has_perm(self, perm, obj=None):
-#         "Does the user have a specific permission?"
-#         # Simplest possible answer: Yes, always
-#         return True
-
-#     def has_module_perms(self, app_label):
-#         "Does the user have permissions to view the app `app_label`?"
-#         # Simplest possible answer: Yes, always
-#         return True
-
-#     @property
-#     def is_staff(self):
-#         "Is the user a member of staff?"
-#         return self.staff
-
-#     @property
-#     def is_admin(self):
-#         "Is the user a admin member?"
-#         return self.admin
+class NGO(CustomUser):
+    description = models.CharField(max_length=500)
