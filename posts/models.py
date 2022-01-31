29e7530fd7a3c6071d8fedc7da59764271961d49
Aniruddha Basak <codewithaniruddha@gmail.com>
2022-01-31 17:44:53 +0530

    Update model attributes in Post models

--- posts/models.py
+++ posts/models.py
@@ -5,21 +5,21 @@ from django.db import models
 from accounts.models import Donner, NGO
 
 
-class Donner_Post(models.Model):
-    user = models.OneToOneField(Donner, on_delete=models.CASCADE)
-    post_id = models.AutoField(primary_key=True)
+class FoodPost(models.Model):
+    user = models.ForeignKey(Donner, on_delete=models.CASCADE)
+    food_post_id = models.AutoField(primary_key=True)
     name = models.CharField(max_length=50)
     food_photo = models.ImageField(upload_to="media/images", null=True, default="")
-    description = models.TextField(blank=True)
-    lon = models.DecimalField(max_digits=22, decimal_places=16, blank=True, null=True)
+    description = models.TextField(blank=True, max_length=500)
     lat = models.DecimalField(max_digits=22, decimal_places=16, blank=True, null=True)
-    received = models.BooleanField(initial=False)
+    lon = models.DecimalField(max_digits=22, decimal_places=16, blank=True, null=True)
+    received = models.BooleanField()
     created_at = models.DateTimeField(auto_now_add=True)
 
 
 class DonationPost(models.Model):
     user = models.ForeignKey(NGO, on_delete=models.CASCADE)
-    id = models.AutoField(primary_key=True)
+    donation_post_id = models.AutoField(primary_key=True)
     campaignName = models.CharField(max_length=50)
     purpose = models.CharField(max_length=500)
     amount = models.PositiveIntegerField()
