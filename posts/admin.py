8fb79b10e2fe5f288493aaaf38956afff70b692a
Aniruddha Basak <codewithaniruddha@gmail.com>
2022-01-31 17:26:49 +0530

    Register Donation post model in the admin panel

--- posts/admin.py
+++ posts/admin.py
@@ -1,5 +1,11 @@
 from django.contrib import admin
+from posts.models import DonationPost
 
+<<<<<<< HEAD
 from .models import Donner_Post
 # Register your models here.
 admin.site.register(Donner_Post)
+=======
+
+admin.site.register(DonationPost)
+>>>>>>> Register Donation post model in the admin panel
