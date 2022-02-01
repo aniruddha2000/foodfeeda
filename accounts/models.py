c614e2bb514ddc5a2654332026d9966a28accd18
Aniruddha Basak <codewithaniruddha@gmail.com>
2022-02-01 15:08:53 +0530

    Refactor Accounts models
    CustomUserModel :
    1. Use AbstractBaseUser
    2. Include address fields
    3. Include phone number fields
    
    NGO :
    1. Add name, ngo_approval_cert fields
    
    Donner :
    1. Add first_name, last_name, gender, coins, DOB, profile_photo fields

--- accounts/models.py
+++ accounts/models.py
@@ -1,82 +1,41 @@
 import datetime
-from enum import unique
-
-from django.contrib.auth.models import AbstractUser
+from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
-from django.utils.timezone import now
-from django.utils.translation import ugettext_lazy as _
-from setuptools import Require
+from django.utils import timezone
+from django.utils.translation import gettext_lazy as _
 from django_countries.fields import CountryField
 from phonenumber_field.modelfields import PhoneNumberField
 
-from .managers import CustomUserManager
+
+from accounts.managers import CustomUserManager
 
 
-class CustomUser(AbstractUser):
-
-    username = None
-    id = models.AutoField(primary_key=True)
+class CustomUser(AbstractBaseUser, PermissionsMixin):
     email = models.EmailField(_("email address"), unique=True)
-    name = models.CharField(max_length=100)
-    is_admin = models.BooleanField(default=False)
-    # is_staff = models.BooleanField(default=False)
-    is_superuser = models.BooleanField(default=False)
-    is_active = models.BooleanField(default=True)
 
-    objects = CustomUserManager()
+    phone_number = PhoneNumberField()
+
+    country = CountryField(blank_label="(select country)")
+    state = models.CharField(max_length=50)
+    city = models.CharField(max_length=100)
+    pin = models.PositiveIntegerField(null=True)
+
+    is_staff = models.BooleanField(default=False)
+    is_active = models.BooleanField(default=True)
+    date_joined = models.DateTimeField(default=timezone.now)
 
     USERNAME_FIELD = "email"
-    REQUIRED_FIELDS = ["name"]
+    REQUIRED_FIELDS = []
 
-    def get_full_name(self):
-        # The user is identified by their email address
-        return self.email
-
-    def get_short_name(self):
-        # The user is identified by their email address
-        return self.email
+    objects = CustomUserManager()
 
     def __str__(self):
         return self.email
 
-    def has_perm(self, perm, obj=None):
-        "Does the user have a specific permission?"
-        # Simplest possible answer: Yes, always
-        return True
-
-    def has_module_perms(self, app_label):
-        "Does the user have permissions to view the app `app_label`?"
-        # Simplest possible answer: Yes, always
-        return True
-
-    # @property
-    # def is_staff(self):
-    #     "Is the user a member of staff?"
-    #     return self.staff
-
-    @property
-    def is_admin(self):
-        "Is the user a admin member?"
-        return self.admin
-
-
-class Address(models.Model):
-    user = models.OneToOneField(CustomUser, on_delete=models.CASCADE)
-    country = CountryField(blank_label="(select country)")
-    state = models.CharField(max_length=50)
-    city = models.CharField(max_length=100)
-    pin = models.PositiveIntegerField()
-
-    def __str__(self) -> str:
-        return f"{self.user} {self.country} {self.state}"
-
-    class Meta:
-        verbose_name = "Address"
-
 
 class NGO(CustomUser):
-    phone_number = PhoneNumberField()
-    address_ngo = Address()
+
+    name = models.CharField(max_length=30)
     ngo_approval_cert = models.FileField()
 
     def __str__(self):
@@ -84,23 +43,23 @@ class NGO(CustomUser):
 
     class Meta:
         verbose_name = "NGO"
-        unique_together = ("phone_number",)
 
 
 GENDER_CHOICES = [("Male", "Male"), ("Female", "Female"), ("Others", "Others")]
 
 
 class Donner(CustomUser):
-    phone_number = PhoneNumberField()
+
+    first_name = models.CharField(max_length=15)
+    last_name = models.CharField(max_length=15)
     gender = models.CharField(max_length=100, choices=GENDER_CHOICES, default=1)
-    address_donner = Address()
     coins = models.IntegerField(default=5)
     DOB = models.DateField(_("DOB"), default=datetime.date.today)
     profile_photo = models.ImageField(upload_to="media/images", null=True, default="")
 
     def __str__(self):
-        return self.name
+        return self.first_name
 
     class Meta:
         verbose_name = "Donner"
-        unique_together = ("phone_number",)
+        # unique_together = ("phone_number",)
