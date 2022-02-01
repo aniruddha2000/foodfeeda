b71cea5e6740525e6601847486c7ef72738e3f19
Aniruddha Basak <codewithaniruddha@gmail.com>
2022-02-01 15:12:40 +0530

    Customize Admin pannel for the accounts models
    Use CustomUserCreationForm and CustomUserChangeForm for all field
    showing in the admin pannel and password hashing in the admin pannel.
    Add list_display items for relevent information

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,17 +1,103 @@
-from pyexpat import model
+from django.contrib.auth.admin import UserAdmin
 from django.contrib import admin
-from .models import CustomUser, NGO, Donner, Address
+from accounts.models import NGO, Donner
+
+from accounts.forms import CustomUserChangeForm, CustomUserCreationForm
 
 
-class AddressInline(admin.StackedInline):
-    model = Address
+class DonnerAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = NGO
+    list_display = (
+        "email",
+        "first_name",
+        "is_staff",
+        "is_active",
+    )
+    list_filter = (
+        "email",
+        "is_staff",
+        "is_active",
+    )
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Permissions", {"fields": ("is_staff", "is_active")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "first_name",
+                    "last_name",
+                    "gender",
+                    "coins",
+                    "DOB",
+                    "email",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_active",
+                    "phone_number",
+                    "country",
+                    "state",
+                    "city",
+                    "pin",
+                    "profile_photo",
+                ),
+            },
+        ),
+    )
+    search_fields = ("email",)
+    ordering = ("email",)
 
 
-class UserAdmin(admin.ModelAdmin):
-    inlines = (AddressInline,)
+class NGOAdmin(UserAdmin):
+    add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
+    model = Donner
+    list_display = (
+        "email",
+        "name",
+        "is_staff",
+        "is_active",
+    )
+    list_filter = (
+        "email",
+        "is_staff",
+        "is_active",
+    )
+    fieldsets = (
+        (None, {"fields": ("email", "password")}),
+        ("Permissions", {"fields": ("is_staff", "is_active")}),
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "name",
+                    "email",
+                    "password1",
+                    "password2",
+                    "is_staff",
+                    "is_active",
+                    "phone_number",
+                    "country",
+                    "state",
+                    "city",
+                    "pin",
+                    "ngo_approval_cert",
+                ),
+            },
+        ),
+    )
+    search_fields = ("email",)
+    ordering = ("email",)
 
 
-admin.site.register(NGO, UserAdmin)
-admin.site.register(Donner, UserAdmin)
-admin.site.register(CustomUser, UserAdmin)
-admin.site.register(Address)
+admin.site.register(NGO, NGOAdmin)
+admin.site.register(Donner, DonnerAdmin)
