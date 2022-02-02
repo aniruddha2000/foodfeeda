c55c1b2b2e504a723701ed470165539057305a09
Aniruddha Basak <codewithaniruddha@gmail.com>
2022-02-02 14:00:07 +0530

    Add FoodPostAdmin panel customization

--- posts/admin.py
+++ posts/admin.py
@@ -46,5 +46,51 @@ class DonationPostAdmin(admin.ModelAdmin):
     ordering = ("donation_post_id",)
 
 
+class FoodPostAdmin(admin.ModelAdmin):
+    model = FoodPost
+    list_display = (
+        "user",
+        "name",
+        "created_at",
+        "received",
+    )
+    list_filter = (
+        "user",
+        "name",
+        "received",
+    )
+    fieldsets = (
+        ("Details", {"fields": ("user", "name", "description", "food_photo")}),
+        ("ID & Date", {"fields": ("food_post_id", "created_at")}),
+        ("Location", {"fields": ("lat", "lon")}),
+        ("Status", {"fields": ("received",)}),
+    )
+    readonly_fields = (
+        "food_post_id",
+        "created_at",
+    )
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": (
+                    "user",
+                    "food_post_id",
+                    "name",
+                    "food_photo",
+                    "description",
+                    "lat",
+                    "lon",
+                    "received",
+                    "created_at",
+                ),
+            },
+        ),
+    )
+    search_fields = ("user",)
+    ordering = ("food_post_id",)
+
+
 admin.site.register(DonationPost, DonationPostAdmin)
-admin.site.register(FoodPost)
+admin.site.register(FoodPost, FoodPostAdmin)
