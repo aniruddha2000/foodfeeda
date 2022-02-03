63b2c13f00921d23eb08a52a6c7f6b561cf47adb
Aniruddha Basak <codewithaniruddha@gmail.com>
2022-02-03 22:19:15 +0530

    Add NGO register serializer

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -58,3 +58,49 @@ class DonnerRegisterSerializer(ModelSerializer):
         user.save()
 
         return user
+
+
+class NGORegisterSerializer(ModelSerializer):
+    email = EmailField(
+        required=True, validators=[UniqueValidator(queryset=Donner.objects.all())]
+    )
+
+    password = CharField(write_only=True, required=True, validators=[validate_password])
+    password2 = CharField(write_only=True, required=True)
+
+    class Meta:
+        model = NGO
+        fields = (
+            "email",
+            "password",
+            "password2",
+            "name",
+            # "phone_number",
+            # "country",
+            # "state",
+            # "city",
+            # "pin",
+            # "ngo_approval_cert",
+        )
+        extra_kwargs = {
+            "name": {"required": True},
+            "password": {"write_only": True},
+            "password2": {"write_only": True},
+        }
+
+    def validate(self, attrs):
+        if attrs["password"] != attrs["password2"]:
+            raise ValidationError({"password": "Password fields didn't match."})
+
+        return attrs
+
+    def create(self, validated_data):
+        user = NGO.objects.create(
+            email=validated_data["email"],
+            name=validated_data["name"],
+        )
+
+        user.set_password(validated_data["password"])
+        user.save()
+
+        return user
