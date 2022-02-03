8d94a24fc9a7b3303f1c27963eda9a6b79e53544
Aniruddha Basak <codewithaniruddha@gmail.com>
2022-02-03 22:19:35 +0530

    Add NGO register url endpoint

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 from rest_framework.authtoken.views import obtain_auth_token
-from accounts.views import DonnerRegisterView
+from accounts.views import DonnerRegisterView, NGORegisterView
 
 urlpatterns = [
     path("auth/login/", obtain_auth_token, name="create-token"),
-    path("auth/donner/register/", DonnerRegisterView.as_view(), name="create-token"),
+    path("auth/donner/register/", DonnerRegisterView.as_view(), name="create-donner"),
+    path("auth/ngo/register/", NGORegisterView.as_view(), name="create-ngo"),
 ]
