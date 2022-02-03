02666bc0fbef929e9ad2904d9ae585de5b3a2811
Aniruddha Basak <codewithaniruddha@gmail.com>
2022-02-03 22:20:01 +0530

    Add NGO register API view

--- accounts/views.py
+++ accounts/views.py
@@ -1,5 +1,5 @@
-from accounts.models import Donner
-from accounts.serializers import DonnerRegisterSerializer
+from accounts.models import Donner, NGO
+from accounts.serializers import DonnerRegisterSerializer, NGORegisterSerializer
 from rest_framework.generics import CreateAPIView
 from rest_framework.permissions import AllowAny
 
@@ -8,3 +8,9 @@ class DonnerRegisterView(CreateAPIView):
     queryset = Donner.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = DonnerRegisterSerializer
+
+
+class NGORegisterView(CreateAPIView):
+    queryset = NGO.objects.all()
+    permission_classes = (AllowAny,)
+    serializer_class = NGORegisterSerializer
