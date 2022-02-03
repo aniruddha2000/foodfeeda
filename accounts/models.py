704ec766f202ab4f8d4787c6dea8d63130258800
Aniruddha Basak <codewithaniruddha@gmail.com>
2022-02-03 21:51:00 +0530

    Add token create on user creation

--- accounts/models.py
+++ accounts/models.py
@@ -5,7 +5,10 @@ from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
 from django_countries.fields import CountryField
 from phonenumber_field.modelfields import PhoneNumberField
-
+from django.conf import settings
+from django.dispatch import receiver
+from django.db.models.signals import post_save
+from rest_framework.authtoken.models import Token
 
 from accounts.managers import CustomUserManager
 
@@ -62,4 +65,9 @@ class Donner(CustomUser):
 
     class Meta:
         verbose_name = "Donner"
-        # unique_together = ("phone_number",)
+
+
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
