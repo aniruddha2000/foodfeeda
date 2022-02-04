8c94ba3e56ac6b1abe6422366757f7429532cab1
Aniruddha Basak <codewithaniruddha@gmail.com>
2022-02-04 17:06:00 +0530

    Update accounts/views.py
    1. MyObtainTokenPairView : for custom JWT tokens
    2. DonnerViewSet : for donner details views
    3. NGOViewSet : for ngo details views

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,37 @@
 from accounts.models import Donner, NGO
-from accounts.serializers import DonnerRegisterSerializer, NGORegisterSerializer
-from rest_framework.generics import CreateAPIView
-from rest_framework.permissions import AllowAny
+from accounts.serializers import (
+    DonnerRegisterSerializer,
+    NGORegisterSerializer,
+    DonnerDetailSerializer,
+    NGODetailSerializer,
+    MyTokenObtainPairSerializer,
+)
+from rest_framework.generics import CreateAPIView, RetrieveAPIView
+from rest_framework.permissions import AllowAny, IsAuthenticated
+
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework_simplejwt.views import TokenObtainPairView
+
+
+class MyObtainTokenPairView(TokenObtainPairView):
+    permission_classes = (AllowAny,)
+    serializer_class = MyTokenObtainPairSerializer
+
+
+class DonnerViewSet(RetrieveAPIView):
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    serializer_class = DonnerDetailSerializer
+    queryset = Donner.objects.all()
+    lookup_field = "id"
+
+
+class NGOViewSet(RetrieveAPIView):
+    authentication_classes = (JWTAuthentication,)
+    permission_classes = (IsAuthenticated,)
+    serializer_class = NGODetailSerializer
+    queryset = NGO.objects.all()
+    lookup_field = "id"
 
 
 class DonnerRegisterView(CreateAPIView):
