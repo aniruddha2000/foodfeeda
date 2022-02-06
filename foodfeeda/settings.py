4ef6bd3ee6387824116b72ba085711f66b6d786a
13KuntalRoy <roykuntal1213@gmail.com>
2022-02-06 16:51:24 +0530

    update foodfeeda
    add urls

--- foodfeeda/settings.py
+++ foodfeeda/settings.py
@@ -41,10 +41,26 @@ INSTALLED_APPS = [
     "donations",
     "django_countries",
     "phonenumber_field",
+    'rest_framework',
+    'corsheaders',
+   
 ]
 
 AUTH_USER_MODEL = "accounts.CustomUser"
-
+REST_FRAMEWORK = {
+    "DEFAULT_RENDERER_CLASSES": (
+        "rest_framework.renderers.JSONRenderer",
+        "rest_framework.renderers.BrowsableAPIRenderer",
+    ),
+    "DEFAULT_AUTHENTICATION_CLASSES": (
+        "rest_framework.authentication.SessionAuthentication",
+        "rest_framework.authentication.BasicAuthentication",
+        "rest_framework.authentication.TokenAuthentication",
+    ),
+    "DEFAULT_PERMISSION_CLASSES": (
+        "rest_framework.permissions.IsAuthenticatedOrReadOnly",
+    ),
+}
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
