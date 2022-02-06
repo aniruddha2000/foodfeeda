cb51270a70ee3476df23e9ac35e8ae0463a8ee3c
Aniruddha Basak <codewithaniruddha@gmail.com>
2022-02-06 13:33:12 +0530

    Add post method in the DonnerRegisterView & NGORegisterView
    Add check if the serializer is valid or not if yes then send the user details with a success messesge and if no then send a failure messesge

--- accounts/views.py
+++ accounts/views.py
@@ -8,6 +8,7 @@ from accounts.serializers import (
 )
 from rest_framework.generics import CreateAPIView, RetrieveAPIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
+from rest_framework.response import Response
 
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework_simplejwt.views import TokenObtainPairView
@@ -39,8 +40,46 @@ class DonnerRegisterView(CreateAPIView):
     permission_classes = (AllowAny,)
     serializer_class = DonnerRegisterSerializer
 
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            user = serializer.save()
+            return Response(
+                {
+                    "user": DonnerDetailSerializer(
+                        user, context=self.get_serializer_context()
+                    ).data,
+                    "status": "Successfully created donner account",
+                }
+            )
+        else:
+            return Response(
+                {
+                    "status": "couldn't create donner account",
+                }
+            )
+
 
 class NGORegisterView(CreateAPIView):
     queryset = NGO.objects.all()
     permission_classes = (AllowAny,)
     serializer_class = NGORegisterSerializer
+
+    def post(self, request, *args, **kwargs):
+        serializer = self.get_serializer(data=request.data)
+        if serializer.is_valid(raise_exception=True):
+            user = serializer.save()
+            return Response(
+                {
+                    "user": NGODetailSerializer(
+                        user, context=self.get_serializer_context()
+                    ).data,
+                    "status": "Successfully created ngo account",
+                }
+            )
+        else:
+            return Response(
+                {
+                    "status": "couldn't create ngo account",
+                }
+            )
