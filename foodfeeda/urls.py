3c3c0b35b32d5dd95cb8d2e83c67ffc8fcd072f8
13KuntalRoy <roykuntal1213@gmail.com>
2022-02-13 15:56:21 +0530

    add base urls

--- foodfeeda/urls.py
+++ foodfeeda/urls.py
@@ -1,10 +1,9 @@
 from django.contrib import admin
 from django.urls import path,include
-
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('razorpay/', include("donations.urls")),
     path("api/", include("accounts.urls")),
-    path('post/',include("posts.urls")),
+    path('api/post/',include("posts.urls")),
     path("api-auth/", include("rest_framework.urls")),
 ]
