a4fa0b0661487706af735022eb66e288e68a861c
Aniruddha Basak <codewithaniruddha@gmail.com>
2022-02-13 18:46:02 +0530

    Remove django countries from settings

--- foodfeeda/settings.py
+++ foodfeeda/settings.py
@@ -40,7 +40,6 @@ INSTALLED_APPS = [
     "rest_framework",
     "rest_framework_simplejwt",
     "rest_framework_simplejwt.token_blacklist",
-    "django_countries",
     "phonenumber_field",
     "accounts",
     "donations",
