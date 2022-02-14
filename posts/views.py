8cc9981ae1ef2a1395b94ff55738f68f7f5f69a0
13KuntalRoy <roykuntal1213@gmail.com>
2022-02-14 16:02:06 +0530

    fix id

--- posts/views.py
+++ posts/views.py
@@ -50,11 +50,10 @@ class PostCreateAPIView(ListCreateAPIView):
     def list(self, request, *args, **kwargs):
         queryset = self.filter_queryset(self.get_queryset())
 
-        page = self.paginate_queryset(queryset)
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
-
+        # page = self.paginate_queryset(queryset)
+        # if page is not None:
+        #     serializer = self.get_serializer(page, many=True)
+        #     return self.get_paginated_response(serializer.data)
         serializer = PostListSerializer(queryset, many=True)
         return Response(serializer.data)
     def get_queryset(self):
