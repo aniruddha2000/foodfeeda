579cad5e4ea0b1a8394bcf10db398c5b20158fea
13KuntalRoy <roykuntal1213@gmail.com>
2022-02-14 15:58:24 +0530

    remove import country

--- accounts/models.py
+++ accounts/models.py
@@ -3,7 +3,6 @@ from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin
 from django.db import models
 from django.utils import timezone
 from django.utils.translation import gettext_lazy as _
-from django_countries.fields import CountryField
 from phonenumber_field.modelfields import PhoneNumberField
 
 from accounts.managers import CustomUserManager
