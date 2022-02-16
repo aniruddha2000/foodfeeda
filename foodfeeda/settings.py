57b190bd72e9b483a17fa1b1fb67a62fd6e50edc
13KuntalRoy <roykuntal1213@gmail.com>
2022-02-16 22:48:09 +0530

    update Settings
    add cors details

--- foodfeeda/settings.py
+++ foodfeeda/settings.py
@@ -117,7 +117,30 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
   "http://127.0.0.1:3000",]
 CORS_ORIGINS_ALLOW_ALL = True 
+CORS_ALLOW_CREDENTIALS = True
+CORS_ALLOW_METHODS = [
+    "DELETE",
+    "GET",
+    "OPTIONS",
+    "PATCH",
+    "POST",
+    "PUT",
+]
+
 CORS_ORIGIN_WHITELIST = ['http://127.0.0.1:3000', 'http://127.0.0.1:8000']
+
+CORS_ALLOW_HEADERS = [
+    "accept",
+    "accept-encoding",
+    "authorization",
+    "content-type",
+    "dnt",
+    "origin",
+    "user-agent",
+    "x-csrftoken",
+    "x-requested-with",
+]
+
 
 ROOT_URLCONF = "foodfeeda.urls"
 
