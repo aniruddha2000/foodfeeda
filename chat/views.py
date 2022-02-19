f2ad89899de6a68a5517a3ccf38168e0037ee496
Aniruddha Basak <codewithaniruddha@gmail.com>
2022-02-19 20:06:16 +0530

    Update chat/views.py
    1. Replace function based views with class based views
    2. Add view for get message by receiver id

--- chat/views.py
+++ chat/views.py
@@ -1,35 +1,44 @@
+from cgitb import lookup
 from django.shortcuts import render
 
-# Django Build in User Model
 from accounts.models import NGO, Donner
 from django.http.response import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 from rest_framework.parsers import JSONParser
-# Our Message model
+
 from chat.models import Message
-from chat.serializers import MessageSerializer
+from chat.serializers import MessageSerializer, MessageSerializerForReceiver
 from rest_framework.decorators import permission_classes, authentication_classes
 from rest_framework.permissions import IsAuthenticated
 from rest_framework_simplejwt.authentication import JWTAuthentication
 
+from rest_framework.generics import ListAPIView, ListCreateAPIView
 
-@csrf_exempt
-@permission_classes((IsAuthenticated,))
-@authentication_classes((JWTAuthentication,))
-def message_list(request, sender=None, receiver=None):
-    """
-    List all required messages, or create a new message.
-    """
-    if request.method == 'GET':
-        messages = Message.objects.filter(
+
+class MessegeList(ListCreateAPIView):
+
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (JWTAuthentication,)
+    serializer_class = MessageSerializer
+
+    def get_queryset(self):
+        sender = self.kwargs['sender']
+        receiver = self.kwargs['receiver']
+
+        queryset = Message.objects.filter(
             sender_id=sender, receiver_id=receiver)
-        serializer = MessageSerializer(
-            messages, many=True, context={'request': request})
-        return JsonResponse(serializer.data, safe=False)
-    elif request.method == 'POST':
-        data = JSONParser().parse(request)
-        serializer = MessageSerializer(data=data)
-        if serializer.is_valid():
-            serializer.save()
-            return JsonResponse(serializer.data, status=201)
-        return JsonResponse(serializer.errors, status=400)
+        return queryset
+
+
+class MessageListByReceiver(ListAPIView):
+
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (JWTAuthentication,)
+    serializer_class = MessageSerializerForReceiver
+
+    def get_queryset(self):
+        # sender = self.kwargs['sender']
+        receiver = self.kwargs['receiver']
+
+        queryset = Message.objects.filter(receiver_id=receiver)
+        return queryset
