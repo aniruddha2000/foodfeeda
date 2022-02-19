3cd4370379346bb991d6b7fb4c59915ba1ac5025
Aniruddha Basak <codewithaniruddha@gmail.com>
2022-02-19 20:08:42 +0530

    Add url for get message by receiver id

--- chat/urls.py
+++ chat/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from chat import views
+# from chat.views import message_list, message_list_receiver
+from chat.views import MessegeList, MessageListByReceiver
+
 urlpatterns = [
-    path('messages/<int:sender>/<int:receiver>',
-         views.message_list, name='message-detail'),
-    path('messages/', views.message_list,
-         name='message-list'),
+    path('messages/<int:sender>/<int:receiver>/',
+         MessegeList.as_view(), name='message-detail'),
+    path('messages/<int:receiver>/', MessageListByReceiver.as_view(),
+         name='message-detail-receiver'),
+    path('messages/', MessegeList.as_view(), name='message-list'),
 ]
