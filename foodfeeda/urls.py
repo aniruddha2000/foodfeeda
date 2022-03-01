ad04074ab15f88bc4f79bb727507a08c379a676e
13KuntalRoy <roykuntal1213@gmail.com>
2022-03-01 22:41:41 +0530

    update foodfeeda settings
    show img

--- foodfeeda/urls.py
+++ foodfeeda/urls.py
@@ -5,7 +5,8 @@ from rest_framework import permissions
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 
-
+from django.conf.urls.static import static
+from django.conf import settings
 schema_view = get_schema_view(
     openapi.Info(
         title="Snippets API",
@@ -32,4 +33,4 @@ urlpatterns = [
         cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc',
         cache_timeout=0), name='schema-redoc'),
-]
+]+static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT) 
